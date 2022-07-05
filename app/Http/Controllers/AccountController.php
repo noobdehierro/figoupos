2cf5163f8ab811a180385bd6147fe4befe737378
noobdehierro <joseluisreyesalvarez97@gmail.com>
2022-07-05 17:29:24 -0500

    crud cuentas listo

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -81,11 +81,17 @@ class AccountController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Account  $account
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function edit(Account $account)
     {
-        //
+
+        return view('adminhtml.accounts.edit', [
+            'account' => $account,
+            'users' => Account::getUsersByBrand(),
+            'brands' => Account::getBrandsByUserBrand()
+        ]);
+
     }
 
     /**
@@ -97,9 +103,25 @@ class AccountController extends Controller
      */
     public function update(Request $request, Account $account)
     {
-        //
-    }
+        $attributes = $request->validate([
+            'user_id' => ['required', Rule::exists('users', 'id')],
+            'brand_id' => ['required', Rule::exists('brands', 'id')],
+            'name' => 'nullable',
+            'amount' => 'nullable'
+        ]);
 
+        try {
+            $attributes['is_active'] = $request->is_active == 'on';
+            $account->update($attributes);
+        } catch (\Exception $exception) {
+            return back()->with('error', $exception->getMessage());
+        }
+
+        return redirect()
+            ->route('accounts.index')
+            ->with('success', 'Se actualizo la cuenta correctamente.');
+
+    }
     /**
      * Remove the specified resource from storage.
      *
@@ -108,7 +130,15 @@ class AccountController extends Controller
      */
     public function destroy(Account $account)
     {
-        //
+        try {
+            $account->delete();
+        } catch (\Exception $exception) {
+            return back()->with('error', $exception->getMessage());
+        }
+
+        return redirect()
+            ->route('accounts.index')
+            ->with('success', 'Se elimino la cuenta correctamente.');
     }
 
     private function checkPermissions()
